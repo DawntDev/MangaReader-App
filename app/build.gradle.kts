62503b65aedf6220d9a9a46199f5851e303dde2a
Dawnt <jmanuelhv9@gmail.com>
2024-07-21 15:05:35 -0600

    Restructure project: Migrate to Android 15 and update Gradle configuration
    This commit includes the following changes:
    - Update Gradle files to Kotlin DSL
    - Set targetSdkVersion to Android 15 (API level 35)
    - Replace deprecated 'kotlin-android-extensions' with View Binding
    - Utilize 'kotlin-parcelize' for Parcelable support

---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("plugin.serialization") version "2.0.0"
+
+}
+
+android {
+    namespace = "com.dawnt.mangareader"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.dawnt.mangareader"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    implementation(libs.androidx.navigation.compose) // Navigation Compose
+    implementation(libs.androidx.datastore.preferences) // DataStore Preferences
+    implementation(libs.kotlinx.serialization.json) // KotlinX Serialization
+    implementation(libs.accompanist.pager) //Carousel
+    implementation(libs.lottie.compose )// Anim Icons
+    implementation(libs.swipe) // Swappable Buttons
+
+    // HttpRequest
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit2.kotlin.coroutines.adapter)
+    implementation(libs.androidx.runtime.livedata)
+
+    // Load Images
+    implementation(libs.coil)
+    implementation (libs.coil.compose)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
